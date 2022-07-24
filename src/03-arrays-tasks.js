fe88243127c0e10ff969794f4b66deef301fe59e
maryna lialik <maryna.lialik@gmail.com>
2022-07-24 20:22:13 +0200

    03-array-tasks completed

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -522,8 +522,15 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const arr = array.map((item) => [
+    keySelector(item),
+    array
+      .filter((item2) => keySelector(item) === keySelector(item2))
+      .map((item3) => valueSelector(item3)),
+  ]);
+  const map = new Map(arr);
+  return map;
 }
 
 /**
@@ -539,8 +546,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat();
 }
 
 /**
@@ -555,8 +562,13 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  if (indexes.length === 1) return arr[indexes[0]];
+
+  return indexes.reduce((previousValue, currentValue, index) => {
+    if (index === 1) return arr[previousValue][currentValue];
+    return previousValue[currentValue];
+  });
 }
 
 /**
@@ -577,8 +589,14 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length === 1) return arr;
+  const floor = Math.floor(arr.length / 2);
+  const ceil = Math.ceil(arr.length / 2);
+  const head = arr.slice(0, floor);
+  const tail = arr.slice(-floor);
+  const middle = ceil === floor ? [] : [ceil];
+  return tail.concat(middle).concat(head);
 }
 
 module.exports = {
